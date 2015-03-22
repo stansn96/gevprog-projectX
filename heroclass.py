0b91806e6b9ce9e7c9af8fd9d2e32171579eef8c
stankorenromp <stankorenromp@hotmail.com>
2015-03-22 04:53:45 -0700

    Aangepaste versie van heroclass.py geupload

--- heroclass.py
+++ heroclass.py
@@ -4,12 +4,29 @@ from spawn import *
 from wumpusclass import *
 
 class Hero():
-	def __init__(self,naam,locatieheld):
+	def __init__(self,naam):
 		self.naam = naam
 		self.pijlen = 3
 		self.gold = 0
 		self.locatieheld = [locatieheld[0],locatieheld[1]]
 		self.winst = False
+		self.plaatsen()
+		
+	def plaatsen(self):
+		self.xcoor = randrange(1,6)
+		self.ycoor = randrange(1,5)
+		self.updatepositie()
+	
+	def herplaatsen(self):
+		self.xcoor = randrange(1,6)
+		self.ycoor = randrange(1,5)
+		if (self.xcoor, self.ycoor) == self.wumpuspositie:
+			self.herplaatsen()
+		else:
+			self.updatepositie()
+			
+	def returnxylocatieheld(self):
+		return self.xcoor, self.ycoor
 		
 	def returnlocatieheld(self):
 		return self.locatieheld
@@ -25,36 +42,39 @@ class Hero():
 		return self.pijlen
 	
 	def schieten(self,wumpuslocatie):
-		self.pijlxcoor = self.locatieheld[0]
-		self.pijlycoor = self.locatieheld[1]
-		self.richting = ""
-		if self.richting == "links":
+		self.pijlxcoor = self.xcoor
+		self.pijlycoor = self.ycoor
+		self.pijlrichting = ""
+		if self.pijlrichting == "links":
 				self.pijlxcoor = self.pijlxcoor - 1
 				if (self.pijlxcoor,self.pijlycoor) == wumpuslocatie:
 					self.winst = True
 				else: 
 					wumpus.respawnwumpus()
 					
-		if self.richting == "rechts":
+		if self.pijlrichting == "rechts":
 				self.pijlxcoor = self.pijlxcoor + 1
 				if (self.pijlxcoor,self.pijlycoor) == wumpuslocatie:
 					self.winst = True
 				else: 
 					wumpus.respawnwumpus()
 					
-		if self.richting == "omhoog":
+		if self.pijlrichting == "omhoog":
 				self.pijlycoor = self.pijlycoor - 1
 				if (self.pijlxcoor,self.pijlycoor) == wumpuslocatie:
 					self.winst = True
 				else: 
 					wumpus.respawnwumpus()
 					
-		if self.richting == "omlaag":
+		if self.pijlrichting == "omlaag":
 				self.pijlycoor = self.pijlycoor + 1
 				if (self.pijlxcoor,self.pijlycoor) == wumpuslocatie:
 					self.winst = True
 				else: 
 					wumpus.respawnwumpus()
+		
+		def __str__(self):
+			return "Naam van de held: {} Positie(x,y) {} Aantal goud: {} Aantal pijlen: {}".format(self.naam,self.positie,self.goud,self.pijlen)
 		
 
 
--- wumpusclass.py
+++ wumpusclass.py
@@ -6,6 +6,8 @@ from random import *
 class Wumpus():
 	def __init__(self, locatiewumpus, locatieheld):
 		self.locatiewumpus = [locatiewumpus[0],locatiewumpus[1]]
+		self.xpos = locatiewumpus[0]
+		self.ypos = locatiewumpus[1]
 		
 	def respawnwumpus(self,locatiewumpus):
 		locatiewumpus = self.locatiewumpus
@@ -24,3 +26,37 @@ class Wumpus():
 			
 	def returnlocatiewumpus(self):
 		return [self.xwumpus,self.ywumpus]
+		
+	def jagen(self,locatieheld):
+		self.xposheld = locatieheld[0][0]
+		self.yposheld = locatieheld[0][1]
+		xafstand = abs(self.xpos - self.xposheld)
+		yafstand = abs(self.ypos - self.yposheld)
+		if self.xpos > self.xposheld:
+			bewegingx = "links"
+		else:
+			bewegingx = "rechts"
+		if self.ypos > self.yposheld:
+			bewegingy = "omhoog"
+		else:
+			bewegingy = "omlaag"
+		if xafstand > yafstand:
+			if bewegingx == "links":
+				self.xpos = self.xpos -1
+			else:
+				self.xpos = self.xpos +1
+		else:
+			if bewegingy == "omhoog":
+				self.ypos = self.ypos -1
+			else:
+				self.ypos = self.ypos +1
+		print(self.xpos,self.ypos)
+		
+def main():
+	wumpus1 = Wumpus((1,1),(2,4))
+	wumpus1.jagen((2,4))
+	wumpus1.jagen((2,4))
+	wumpus1.jagen((2.4))
+	wumpus1.jagen((2.4))
+	
+main()
